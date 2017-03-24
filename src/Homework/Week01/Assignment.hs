e43f6a8d1cb0178c92c213335825b9659de997d4
Michael Avila <me@michaelavila.com>
2017-03-24 11:13:29 -0700

    Add solutions for week 1

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,30 +2,39 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits = reverse . toDigitsRev
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n
+  | n > 0 = (n `mod` 10):toDigitsRev (n `div` 10)
+  | otherwise = []
 
 -- #2
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' = zipWith (*) (cycle [1,2])
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleEveryOther' . reverse
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits = sum . concat . map toDigits
 
 -- #4
+isDivisibleByTen :: Integer -> Bool
+isDivisibleByTen = (==0) . (`mod`10)
+
 validate :: Integer -> Bool
-validate = undefined
+validate = isDivisibleByTen . sumDigits . doubleEveryOther . toDigits
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
