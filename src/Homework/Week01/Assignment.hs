dae2aa3e77d820f8f8c298625d23e18e6c4816b5
Sidney Keese <sidke.z@gmail.com>
2017-03-24 11:13:41 -0700

    committing homework is an alright idea

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,30 +2,38 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n | n <= 0    = []
+           | otherwise = toDigits(div n 10) ++ [mod n 10]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n = reverse (toDigits n)
 
 -- #2
+thing :: [Integer] -> [Integer]
+thing (x:y:zs) = [x, y * 2] ++ thing zs
+thing n = n
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . thing . reverse
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+-- sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+sumDigits xs = foldr ((+) . sum . toDigits) 0 xs
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = mod (sumDigits (doubleEveryOther (toDigits n))) 10 == 0
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi n a b c | n == 0     = []
+              | otherwise  = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
--- test/Homework/Week01Spec.hs
+++ test/Homework/Week01Spec.hs
@@ -13,89 +13,70 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "toDigits" $ do
-    it "should split digits of integer into a list" $ do
-      pending
+    it "should split digits of integer into a list" $
       toDigits 1234 `shouldBe` [1,2,3,4]
 
-    it "should return an empty list for zero" $ do
-      pending
+    it "should return an empty list for zero" $
       toDigits 0 `shouldBe` []
 
-    it "should return an empty list for negative numbers" $ do
-      pending
+    it "should return an empty list for negative numbers" $
       toDigits (-1) `shouldBe` []
 
   describe "toDigitsRev" $ do
-    it "should return an empty list for zero" $ do
-      pending
+    it "should return an empty list for zero" $
       toDigitsRev 0 `shouldBe` []
 
     it "should return an empty list for negative numbers" $ do
-      pending
       toDigitsRev (-1) `shouldBe` []
       toDigitsRev (-22222) `shouldBe` []
 
     it "should split digits of integer into a list in reverse order" $ do
-      pending
       toDigitsRev 123 `shouldBe` [3,2,1]
       toDigitsRev 431 `shouldBe` [1,3,4]
       toDigitsRev 12 `shouldBe` [2,1]
       toDigitsRev 2 `shouldBe` [2]
 
   describe "doubleEveryOther" $ do
-    it "should return an empty list given an empty list" $ do
-      pending
+    it "should return an empty list given an empty list" $
       doubleEveryOther [] `shouldBe` []
 
     it "should double every other int in the list, from right to left" $ do
-      pending
       doubleEveryOther [8,7,6,5] `shouldBe`[16,7,12,5]
       doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
 
   describe "sumDigits" $ do
-    it "should return zero for an empty list" $ do
-      pending
+    it "should return zero for an empty list" $
       sumDigits [] `shouldBe` 0
 
     it "should sum all digits in the list" $ do
-      pending
       sumDigits [16,7,12,5] `shouldBe` 22
       sumDigits [18,7,33,5] `shouldBe` 27
 
   describe "validate" $ do
-    it "should return True for valid card number" $ do
-      pending
+    it "should return True for valid card number" $
       validate 4012888888881881 `shouldBe` True
 
-    it "should return False for invalid card number" $ do
-      pending
+    it "should return False for invalid card number" $
       validate 4012888888881882 `shouldBe` False
 
   describe "hanoi" $ do
-    it "should return an empty list for zero discs" $ do
-      pending
+    it "should return an empty list for zero discs" $
       hanoi 0 "a" "b" "c" `shouldBe` []
 
-    it "should solve for 1 disc" $ do
-      pending
+    it "should solve for 1 disc" $
       hanoi 1 "a" "b" "c" `shouldBe` [("a", "b")]
 
-    it "should solve for 2 discs" $ do
-      pending
+    it "should solve for 2 discs" $
       hanoi 2 "a" "b" "c" `shouldBe` [("a", "c"), ("a", "b"), ("c", "b")]
 
-    it "should solve for 3 discs" $ do
-      pending
+    it "should solve for 3 discs" $
       hanoi 3 "a" "b" "c" `shouldBe` [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")]
 
-    it "should have the correct number of moves for 4 discs" $ do
-      pending
-      (length $ hanoi 4 "a" "b" "c") `shouldBe` 15
+    it "should have the correct number of moves for 4 discs" $
+      length (hanoi 4 "a" "b" "c") `shouldBe` 15
 
-    it "should have the correct number of moves for 5 discs" $ do
-      pending
-      (length $ hanoi 5 "a" "b" "c") `shouldBe` 31
+    it "should have the correct number of moves for 5 discs" $
+      length (hanoi 5 "a" "b" "c") `shouldBe` 31
 
-    it "should have the correct number of moves for 6 discs" $ do
-      pending
-      (length $ hanoi 6 "a" "b" "c") `shouldBe` 63
+    it "should have the correct number of moves for 6 discs" $
+      length (hanoi 6 "a" "b" "c") `shouldBe` 63
