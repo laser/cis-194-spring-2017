6e2310d5cd7819c277f40d6d048f667320760e7e
Michael Avila <me@michaelavila.com>
2017-03-25 21:08:46 -0700

    Week 2: Reduce applicative operator count

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -21,23 +21,26 @@ import Text.Parsec (many1, digit, anyChar, space, char, (<|>), ParseError)
 import Text.Parsec.String (Parser)
 
 -- #1a
-
 infoMessage :: Parser LogMessage
 infoMessage = do
-  char 'I' >> space
+  char 'I'
+  space
   (ts, msg) <- timeStampedMessage
   return $ LogMessage Info ts msg
 
 warningMessage :: Parser LogMessage
 warningMessage = do
-  char 'W' >> space
+  char 'W'
+  space
   (ts, msg) <- timeStampedMessage
   return $ LogMessage Warning ts msg
 
 errorMessage :: Parser LogMessage
 errorMessage = do
-  char 'E' >> space
-  c <- int <* space
+  char 'E'
+  space
+  c <- int
+  space
   (ts, msg) <- timeStampedMessage
   return $ LogMessage (Error c) ts msg
 
@@ -49,7 +52,7 @@ logMessage = infoMessage <|> warningMessage <|> errorMessage <|> unknownMessage
 
 parseMessage :: String -> LogMessage
 parseMessage msg = case Parsec.parse logMessage "" msg of
-                     (Right lm) -> lm
+                     (Right m) -> m
                      (Left _) -> error "Something has gone terribly wrong"
 
 -- #1b
@@ -66,7 +69,7 @@ insert m@(LogMessage _ mts _) (Node left mt@(LogMessage _ mtts _) right)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = foldl (\acc m -> insert m acc) Leaf
+build = foldl (flip insert) Leaf
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
@@ -86,16 +89,16 @@ whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong = fmap toMessage . filter isRelevant . inOrder . build
 
 -- parser combinator helpers
-
 int :: Parser Int
-int = (many1 digit) >>= return . read
+int = many1 digit >>= return . read
 
 allChars :: Parser String
 allChars = many1 anyChar >>= return
 
 timeStampedMessage :: Parser (TimeStamp, String)
 timeStampedMessage = do
-  ts <- int <* space
+  ts <- int
+  space
   msg <- allChars
   return (ts, dropWhileEnd isSpace msg)
 
