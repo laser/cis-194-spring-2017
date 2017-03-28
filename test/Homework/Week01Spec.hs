3c7fe1d5c1e5b129ef497764019c72678a5ea994
Rob Pak <rob@carbonfive.com>
2017-03-28 10:31:44 -0700

    Introduce intellij support

--- test/Homework/Week01Spec.hs
+++ test/Homework/Week01Spec.hs
@@ -18,7 +18,6 @@ spec = do
       toDigits 1234 `shouldBe` [1,2,3,4]
 
     it "should return an empty list for zero" $ do
-      pending
       toDigits 0 `shouldBe` []
 
     it "should return an empty list for negative numbers" $ do
