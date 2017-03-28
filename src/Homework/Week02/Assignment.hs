afae0ef64d06d8d73c31498adf1156351d7107d1
Rudy Jahchan <rudy@carbonfive.com>
2017-03-27 22:46:15 -0700

    Finished Homework 02

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,24 +15,41 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage s = let w = words s
+                 in case w of
+                   ("E" : priority : timestamp : message) -> LogMessage (Error (read priority)) (read timestamp) (unwords message)
+                   ("I" : timestamp : message) -> LogMessage Info (read timestamp) (unwords message)
+                   ("W" : timestamp : message) -> LogMessage Warning (read timestamp) (unwords message)
+                   _ -> Unknown (unwords w)
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse = (map parseMessage) . lines
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) _ = Leaf
+insert message Leaf = Node Leaf message Leaf
+insert m@(LogMessage _ timestamp _) (Node left o@(LogMessage _ timestamp2 _) right)
+        | timestamp < timestamp2 = Node (insert m left) o right
+        | timestamp > timestamp2 = Node left o (insert m right)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build ms = build' ms Leaf
+        where build' [] acc = acc
+              build' (m:ms) acc = build' ms (insert m acc)
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left m right) = (inOrder left) ++ [m] ++ (inOrder right)
 
 -- #5
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong = (map message) . inOrder . build . (filter highPriority)
+        where highPriority m = (priority m) > 50
+              priority (LogMessage (Error p) _ _) = p
+              priority _ = 0
+              message (LogMessage _ _ m) = m
+              message (Unknown m) = m
