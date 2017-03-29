856950cd8cb913386821607d13452d7a4ff68f4e
bentona <benton.anderson@gmail.com>
2017-03-28 22:13:09 -0700

    Assignment 01 (w/o hanoi)

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,23 +2,36 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits 0 = []
+toDigits n | n < 0 = []
+toDigits n = toDigits(n `div` 10) ++ [n `mod` 10]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev 0 = []
+toDigitsRev n | n < 0 = []
+toDigitsRev n = n `mod` 10 :  toDigitsRev(n `div` 10)
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther [] = []
+doubleEveryOther a | even (length a) = [head a * 2] ++ doubleEveryOther(tail a)
+doubleEveryOther a = [head a] ++ doubleEveryOther(tail a)
+
 
 -- #3
+flatDigits :: [Integer] -> [Integer]
+flatDigits = concatMap toDigits
+
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits = sum . flatDigits
 
 -- #4
+isDivZero :: Integer -> Bool
+isDivZero n =  0 == n `mod` 10
+
 validate :: Integer -> Bool
-validate = undefined
+validate = isDivZero . sumDigits . doubleEveryOther . toDigits
 
 -- #5
 type Peg = String
