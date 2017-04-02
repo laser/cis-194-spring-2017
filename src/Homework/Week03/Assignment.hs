5d4faac48fffbd0d7b82abd7e750a14dd0f1812b
Rudy Jahchan <rudy@carbonfive.com>
2017-04-02 10:05:06 -0700

    Homework03

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,14 +4,28 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Data.List
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips xs = map ((flip s) xs) [1..length xs]
+  where s n = map snd . filter fst . zip (cycle (replicate (n - 1) False ++ [True]))
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima = map m . filter mx . ts
+  where m (_, x, _) = x
+        mx (x, y, z)
+          | x < y && y > z = True
+          | otherwise = False
+        ts (x : xs@(y : z : _)) = (x, y, z) : ts xs
+        ts _ = []
 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram xs = intercalate "\n" $ reverse(hr) ++ ["==========\n0123456789\n"]
+  where hr = map ((flip ss) cs) [1..maximum cs]
+        cs = map ((flip c) xs) [0..9]
+        c n = length . filter (== n)
+        ss n = map (s n)
+        s n x = if x >= n then '*' else ' '
