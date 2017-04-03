eeb0593c963593e255f6bb2188cd68830a199ada
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-04-03 02:05:13 -0700

    long implementation of skips

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -1,12 +1,20 @@
-module Homework.Week03.Assignment (
-  skips,
-  localMaxima,
-  histogram
-) where
+module Homework.Week03.Assignment
+  ( skips
+  , localMaxima
+  , histogram
+  ) where
 
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips xs = map (skipsN [] xs) [1 .. (length xs)]
+
+skipsN :: [a] -> [a] -> Int -> [a]
+skipsN acc xs n
+  | length xs < n = reverse acc
+  | otherwise =
+    let nth:_ = (reverse . take n $ xs)
+        rest = drop n xs
+    in skipsN (nth : acc) rest n
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
