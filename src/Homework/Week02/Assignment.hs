fbd2e51de9edce5402134df74be4996c2750fb3e
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-04-02 22:36:59 -0700

    parse and insert for 02

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -1,30 +1,59 @@
-module Homework.Week02.Assignment (
-  build,
-  inOrder,
-  insert,
-  parse,
-  parseMessage,
-  whatWentWrong,
-  LogMessage(..),
-  MessageTree(..),
-  MessageType(..),
-  TimeStamp
-) where
+module Homework.Week02.Assignment
+  ( build
+  , inOrder
+  , insert
+  , parse
+  , parseMessage
+  , whatWentWrong
+  , LogMessage(..)
+  , MessageTree(..)
+  , MessageType(..)
+  , TimeStamp
+  ) where
 
 import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage ('E':' ':message) =
+  let error_code_string:messageText = words message
+      error_code = read error_code_string :: Int
+  in parseMessage' (Error error_code) (unwords messageText)
+parseMessage ('I':' ':message) = parseMessage' Info message
+parseMessage ('W':' ':message) = parseMessage' Warning message
+parseMessage message = Unknown message
+
+parseMessage' :: MessageType -> String -> LogMessage
+parseMessage' messageType message =
+  let time_string:message_words = words message
+      time = read time_string :: Int
+      messageText = unwords message_words
+  in LogMessage messageType time messageText
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse logs =
+  let messages = lines logs
+  in map parseMessage messages
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) messageTree = messageTree
+insert logMessage Leaf = Node Leaf logMessage Leaf
+insert logMessage messageTree@(Node leftTree nodeMessage rightTree)
+  | nodeMessage == logMessage = messageTree
+  | time < nodeTime = Node (insert logMessage leftTree) nodeMessage rightTree
+  | otherwise = Node leftTree nodeMessage (insert logMessage rightTree)
+  where
+    time = logTime logMessage
+    nodeTime = logTime nodeMessage
 
+logTime :: LogMessage -> Int
+logTime (LogMessage _ time _) = time
+logTime (Unknown _) = -1
+
+-- insert messageTree
+--   | 
 -- #3
 build :: [LogMessage] -> MessageTree
 build = undefined
