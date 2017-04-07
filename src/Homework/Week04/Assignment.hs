aeb8bedf97a13bbcc9f14104b21d568cb988b334
Rudy Jahchan <rudy@carbonfive.com>
2017-04-07 08:21:29 -0700

    Homework 4

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -21,25 +21,28 @@ module Homework.Week04.Assignment (
 
 import Homework.Week04.BST
 
+import Data.Maybe
+import Data.List
+
 -- #1
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 = error "impossible"
 
 -- #2
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 x y = x
 
 -- #3
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 _ x = x
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 _ _ x = x
 
 -- #5
 ex5 :: Bool -> Bool
-ex5 = undefined
+ex5 x = x
 
 -- #6
 ex6 :: (a -> a) -> a
@@ -47,44 +50,61 @@ ex6 = undefined
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 f y = f y
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 xs = xs
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+ex9 f xs = map f xs
 
 -- #10
 ex10 :: Maybe a -> a
-ex10 = undefined
+ex10 = error "impossible"
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 x = Just x
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+ex12 x = x
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ message Leaf = Node Leaf message Leaf
+insertBST cmp x (Node left y right)
+        | cmp x y == LT = Node (insertBST cmp x left) y right
+        | cmp x y == GT = Node left y (insertBST cmp x right)
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (_:xs) = Just xs
 
 -- #14
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps [] = True
+allCaps xs = all cap $ map safeHead xs
+  where cap (Just x) = elem x ['A'..'Z']
+        cap _ = False
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+dropTrailingWhitespace = foldr d ""
+  where d a b
+          | a == ' ' && isNothing (safeHead b) = b
+          | otherwise = a : b
 
 -- #16
 firstLetters :: [String] -> [Char]
-firstLetters = undefined
+firstLetters xs = map fromJust $ filter isJust $ map safeHead xs
 
 -- #17
 asList :: [String] -> String
-asList = undefined
+asList xs = "[" ++ (intercalate "," xs) ++ "]"
