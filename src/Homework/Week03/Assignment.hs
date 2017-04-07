0fd55af948c7390eee3e2c9d2523268332da0058
Rob Pak <rob@carbonfive.com>
2017-04-07 12:18:23 -0700

    Skips implementation

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,9 +4,24 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Prelude
+
+skips' :: [a] -> Int -> [a]
+skips' _ 0 = []
+skips' list n
+  | null list = []
+  | length list == n = [last list]
+  | length list < n = []
+  | otherwise = do
+    let split = splitAt n list
+    last (fst split) : skips' (snd split) n
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips list@(x : xs)
+  | null xs = [[x]]
+  | otherwise = map (skips' list) [1..length list]
+skips _ = []
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
