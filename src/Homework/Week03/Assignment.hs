1e53043286ad974b9b9e7e9e0db4c7f30f25013b
Will Ockelmann-Wagner <wow@carbonfive.com>
2017-04-06 23:04:24 -0700

    solve golf 2

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,6 +4,8 @@ module Homework.Week03.Assignment
   , histogram
   ) where
 
+import Data.List
+
 -- #1
 skips :: [a] -> [[a]]
 skips xs = map (skipsN [] xs) [1 .. (length xs)]
@@ -18,8 +20,18 @@ skipsN acc xs n
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima xs = localMaxima' xs []
+
+localMaxima' :: [Integer] -> [Integer] -> [Integer]
+localMaxima' xs acc
+  | length xs < 3 = reverse acc
+localMaxima' (before:x:after:xs) acc
+  | before < x && x > after = localMaxima' (x : after : xs) (x : acc)
+  | otherwise = localMaxima' (x : after : xs) acc
 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram xs = histogram_printer (Data.List.sort xs) []
+
+histogram_printer :: [Integer] -> [String] -> String
+histogram_printer xs graph = undefined
