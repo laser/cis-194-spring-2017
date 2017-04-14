5295207215d466cb6fdcc3fe8c378cdf3b1d717c
Rudy Jahchan <rudy@carbonfive.com>
2017-04-13 21:30:22 -0700

    Week06 complete

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -13,37 +13,44 @@ module Homework.Week06.Assignment (
 
 -- #1a
 fib :: Integer -> Integer
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib x = (fib (x - 1)) + (fib (x -2))
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [0..]
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = fibs' (-1) 1
+  where fibs' x y = z : (fibs' y z)
+          where z = x + y
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Stream a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Stream x xs) = x : streamToList xs
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where
+  show = show . (take 20) . streamToList
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat x = Stream x (streamRepeat x)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Stream x xs) = Stream (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f a = Stream a (streamFromSeed f (f a))
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Stream x xs) ys = Stream x (interleaveStreams ys xs)
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = foldr1 interleaveStreams $ map streamRepeat [0..]
