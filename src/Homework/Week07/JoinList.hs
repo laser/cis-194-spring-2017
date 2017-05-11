f04b72e9a6c91638f24dbf96d40b3f3abdfd6af1
Chhi'med Kunzang <chhimed@carbonfive.com>
2017-05-10 22:28:03 -0700

    Fixes from class help.

--- src/Homework/Week07/JoinList.hs
+++ src/Homework/Week07/JoinList.hs
@@ -3,12 +3,17 @@
 module Homework.Week07.JoinList where
 
 import Homework.Week07.Buffer
+import Homework.Week07.Sized
 import Homework.Week07.Scrabble
 import Homework.Week07.Sized
 
+import Data.Monoid
+
 data JoinList m a = Empty
                   | Single m a
-                  | Append m (JoinList m a) (JoinList m a) deriving (Eq, Show)
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
 
 tag :: Monoid m => JoinList m a -> m
 tag = undefined
@@ -41,3 +46,37 @@ instance Buffer (JoinList (Score, Size) String) where
   replaceLine = undefined
   toString = undefined
   value = undefined
+
+tag :: Monoid m => JoinList m a -> m
+--tag Empty = Empty
+tag (Single m _) = m
+tag (Append m _ _) = m
+tag Empty = mempty
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) Empty x = x
+(+++) x Empty = x
+(+++) l r = Append ((tag l) <> (tag r)) l r
+
+indexJ :: (Sized b, Monoid b) =>
+  Int -> JoinList b a -> Maybe a  
+indexJ i Empty = Nothing
+indexJ i (Single  x j)
+  | i < 0 = Nothing
+  | i == getSize (size x) = Just j
+-- indexJ i (Append x l r)
+--   | i < x = indexJ i l
+  -- | i >= x = indexJ (i - x) r
+
+--dropJ :: (Sized b, Monoid b) =>
+--  Int -> JoinList b a -> JoinList b a
+
+  
+--takeJ :: (Sized b, Monoid b) =>
+--  Int -> JoinList b a -> JoinList b a
+
