07525153d56819b9c4bd57c962a605229c3b6000
Chhi'med Kunzang <chhimed@carbonfive.com>
2017-05-11 00:23:00 -0700

    Implement indexJ, dropJ, and takeJ.

--- src/Homework/Week07/JoinList.hs
+++ src/Homework/Week07/JoinList.hs
@@ -16,25 +16,58 @@ data JoinList m a = Empty
 
 
 tag :: Monoid m => JoinList m a -> m
-tag = undefined
+--tag Empty = Empty
+tag (Single m _) = m
+tag (Append m _ _) = m
+tag Empty = mempty
 
 (!!?) :: [a] -> Int -> Maybe a
 (!!?) = undefined
 
 jlToList :: Monoid m => JoinList m a -> [a]
-jlToList = undefined
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-(+++) = undefined
+(+++) Empty x = x
+(+++) x Empty = x
+(+++) l r = Append ((tag l) <> (tag r)) l r
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ = undefined
+indexJ i Empty = Nothing
+indexJ i (Single x j)
+  | i == 0 = Just j
+  | otherwise = Nothing
+  where xx = getSize (size x) 
+indexJ i (Append x l r)
+  | i < xx = indexJ i l
+  | i >= xx = indexJ (i - xx) r
+  where xx = getSize (size (tag l))
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ = undefined
+dropJ i Empty = Empty
+dropJ i jl@(Single x j)
+  | i == 0 = jl
+  | otherwise = Empty
+  where xx = getSize (size x)
+dropJ i jl@(Append x l r)
+  | i == 0 = jl
+  | i < xx = (dropJ i l) +++ r
+  | i >= xx = dropJ (i - xx) r
+  where xx = getSize (size (tag l))
+
 
 takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-takeJ = undefined
+takeJ i Empty = Empty
+takeJ i jl@(Single x j)
+  | i <= 0 = Empty
+  | otherwise = jl
+takeJ i jl@(Append x l r)
+  | i < xx = takeJ i l
+  | otherwise = l +++ (takeJ (i - xx) r)
+  where xx = getSize (size (tag l))
 
 scoreLine :: String -> JoinList Score String
 scoreLine = undefined
@@ -47,31 +80,6 @@ instance Buffer (JoinList (Score, Size) String) where
   toString = undefined
   value = undefined
 
-tag :: Monoid m => JoinList m a -> m
---tag Empty = Empty
-tag (Single m _) = m
-tag (Append m _ _) = m
-tag Empty = mempty
-
-jlToList :: JoinList m a -> [a]
-jlToList Empty = []
-jlToList (Single _ a) = [a]
-jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
-
-(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-(+++) Empty x = x
-(+++) x Empty = x
-(+++) l r = Append ((tag l) <> (tag r)) l r
-
-indexJ :: (Sized b, Monoid b) =>
-  Int -> JoinList b a -> Maybe a  
-indexJ i Empty = Nothing
-indexJ i (Single  x j)
-  | i < 0 = Nothing
-  | i == getSize (size x) = Just j
--- indexJ i (Append x l r)
---   | i < x = indexJ i l
-  -- | i >= x = indexJ (i - x) r
 
 --dropJ :: (Sized b, Monoid b) =>
 --  Int -> JoinList b a -> JoinList b a
