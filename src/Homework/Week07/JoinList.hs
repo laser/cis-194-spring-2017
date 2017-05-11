52a05ffc2670ce02be36d46256ff1b2d8a3253ad
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-05-11 01:38:57 -0700

    got appending working, trying to test indexJ

--- src/Homework/Week07/JoinList.hs
+++ src/Homework/Week07/JoinList.hs
@@ -22,19 +22,32 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a) deriving (Eq, Show)
 
 tag :: Monoid m => JoinList m a -> m
-tag = undefined
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
 
 (!!?) :: [a] -> Int -> Maybe a
-(!!?) = undefined
+(!!?) [] _ = Nothing
+(!!?) _ i | i < 0 = Nothing
+(!!?) (x:xs) 0 = Just x
+(!!?) (x:xs) i = xs !!? (i-1)
 
 jlToList :: Monoid m => JoinList m a -> [a]
-jlToList = undefined
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-(+++) = undefined
+(+++) list1 list2 = Append (mappend (tag list1) (tag list2)) list1 list2
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ = undefined
+indexJ _ Empty = Nothing
+indexJ i _ | i < 0 = Nothing
+indexJ _ (Single _ a) = Just a
+indexJ i (Append _ l1 l2)
+  | i < index1 = indexJ i l1 
+  | otherwise = indexJ i l2
+  where index1 = getSize . size . tag $ l1
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ = undefined
