2ed9ae4d0f84398f289e5d4888c20bbe8cc6597a
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-05-16 01:36:50 -0700

    exercise 2 tests

--- test/Homework/Week07Spec.hs
+++ test/Homework/Week07Spec.hs
@@ -29,21 +29,37 @@ spec = do
       let b = (Single (Sum 2) 'b')
       (+++) a b `shouldBe` (Append (Sum 5) a b)
 
-  describe "indexJ" $ do
-    it "does what you tell it to do" $ do
-      pending
-      let jl = (Append (Size 3) (Append (Size 2) (Single (Size 1) 's') (Single (Size 1) 'u')) (Single (Size 1) 'p'))
-      indexJ 0 jl `shouldBe` Just 's'
-      indexJ 1 jl `shouldBe` Just 'u'
-      indexJ 2 jl `shouldBe` Just 'p'
+  describe "exercise 2" $ do
+    let jl = (Append (Size 3) (Append (Size 2) (Single (Size 1) 's') (Single (Size 1) 'u')) (Single (Size 1) 'p'))
 
-  describe "dropJ" $ do
-    it "does what you tell it to do" $ do
-      pending
+    describe "indexJ" $ do
+      it "finds a letter at the specified index" $ do
+        pending
+        (indexJ 0 jl) `shouldBe` (Just 's')
+        (indexJ 1 jl) `shouldBe` (Just 'u')
+        (indexJ 2 jl) `shouldBe` (Just 'p')
 
-  describe "takeJ" $ do
-    it "does what you tell it to do" $ do
-      pending
+    describe "dropJ" $ do
+      it "returns the list when dropping 0" $ do
+        pending
+        (dropJ 0 jl) `shouldBe` jl
+      it "drops the first element from a JoinList" $ do
+        pending
+        (dropJ 1 jl) `shouldBe` Append (Size 2) (Single (Size 1) 'u') (Single (Size 1) 'p')
+      it "drops the first n elements from a JoinList" $ do
+        pending
+        (dropJ 2 jl) `shouldBe` Single (Size 1) 'p'
+
+    describe "takeJ" $ do
+      it "returns the list when taking more than the list" $ do
+        pending
+        (takeJ 4 jl) `shouldBe` jl
+      it "takes the first element from a JoinList" $ do
+        pending
+        (takeJ 1 jl) `shouldBe` Single (Size 1) 's'
+      it "takes the first n elements from a JoinList" $ do
+        pending
+        (takeJ 2 jl) `shouldBe` Append (Size 2) (Single (Size 1) 's') (Single (Size 1) 'u')
 
   describe "scoreLine" $ do
     it "does what you tell it to do" $ do
