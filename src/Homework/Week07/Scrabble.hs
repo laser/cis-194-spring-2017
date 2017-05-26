6c8146408efa2f4f66a6df1aa3f21a1e74294e3c
Will Ockelmann-Wagner <wow@carbonfive.com>
2017-05-25 19:35:49 -0700

    add scrabble scoring

--- src/Homework/Week07/Scrabble.hs
+++ src/Homework/Week07/Scrabble.hs
@@ -8,7 +8,34 @@ instance Monoid Score where
   mappend = undefined
 
 score :: Char -> Score
-score = undefined
+score 'a' = Score 1
+score 'b' = Score 3
+score 'c' = Score 3
+score 'd' = Score 2
+score 'e' = Score 1
+score 'f' = Score 4
+score 'g' = Score 2
+score 'h' = Score 4
+score 'i' = Score 1
+score 'j' = Score 8
+score 'k' = Score 5
+score 'l' = Score 1
+score 'm' = Score 3
+score 'n' = Score 1
+score 'o' = Score 1
+score 'p' = Score 3
+score 'q' = Score 10
+score 'r' = Score 1
+score 's' = Score 1
+score 't' = Score 1
+score 'u' = Score 1
+score 'v' = Score 4
+score 'w' = Score 4
+score 'x' = Score 8
+score 'y' = Score 4
+score 'z' = Score 10
+score _ = Score 0
 
 scoreString :: String -> Score
-scoreString = undefined
+scoreString "" = Score 0
+scoreString (x:xs) = mappend (score x) (scoreString xs)
