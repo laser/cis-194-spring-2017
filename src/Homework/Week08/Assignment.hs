fd061a2a82d1041d04f6405249329d6975207967
Rudy Jahchan <rudy@carbonfive.com>
2017-06-11 19:01:18 -0700

    Week08 Complete!

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -8,34 +8,42 @@ module Homework.Week08.Assignment (
 
 import Homework.Week08.AParser
 import Control.Applicative
+import Data.Char
 
 -- #1
 first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+first f (a, c) = (f a, c)
 
 instance Functor Parser where
-  fmap = undefined
+  fmap f (Parser runParser) = Parser $ ((first f) <$>) . runParser
 
 -- #2
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure a = Parser f
+    where f xs = Just(a, xs)
+  Parser rp1 <*> Parser rp2 = Parser f
+        where
+          f xs = case (rp1 xs) of
+            Just (rf1, rxs) -> case rp2 rxs of
+              Just (rv2, s) -> Just (rf1 rv2, s)
+              otherwise -> Nothing
+            otherwise -> Nothing
 
 -- #3
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = (,) <$> char 'a' <*> char 'b'
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = const () <$> abParser
 
 intPair :: Parser [Integer]
-intPair = undefined
+intPair = (\(a, b) -> [a,b]) <$> ((,) <$> posInt <* char ' ' <*> posInt)
 
 -- #4
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = Parser (const Nothing)
+  Parser rp1 <|> Parser rp2 = Parser $ (<|>) <$> rp1 <*> rp2
 
 -- #5
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = (const () <$> posInt) <|> (const () <$> satisfy isUpper)
