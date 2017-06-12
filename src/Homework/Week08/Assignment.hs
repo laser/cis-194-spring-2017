b46b96c5627485ba3cb1b52712b374208c08690c
Michael Avila <me@michaelavila.com>
2017-06-12 06:22:11 -0700

    Add week08 assignment

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -3,39 +3,55 @@ module Homework.Week08.Assignment (
   abParser,
   abParser_,
   intPair,
-  intOrUppercase
+  intOrUppercase,
+  satisfy,
+  posInt,
+  Parser
 ) where
 
 import Homework.Week08.AParser
 import Control.Applicative
 
+import Data.Char
+
 -- #1
 first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+first f (a,c) = (f a,c)
 
 instance Functor Parser where
-  fmap = undefined
-
+  fmap f p = Parser (fmap (fmap (first f)) (runParser p))
+  
 -- #2
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure a = Parser (\s -> Just (a,s))
+  p <*> g = Parser (\s ->
+    case runParser p s of
+      Nothing       -> Nothing
+      Just (f',s')  -> runParser (fmap f' g) s')
 
 -- #3
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = Parser (\s ->
+  case s of
+    ('a':'b':s') -> Just (('a', 'b'), s')
+    _ -> Nothing)
+
+abParser' :: Parser (Char, Char)
+abParser' = (,) <$> char 'a' <*> char 'b'
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = (const (const  ())) <$> char 'a' <*> char 'b'
 
 intPair :: Parser [Integer]
-intPair = undefined
+intPair = (\a _ b -> [a,b]) <$> posInt <*> char ' ' <*> posInt
 
 -- #4
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = Parser (const Nothing)
+  Parser p <|> Parser g = Parser (pure (<|>) <*> p <*> g)
 
 -- #5
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = (unit' <$> posInt) <|> (unit' <$> (satisfy isUpper))
+  where unit' = const ()
+
