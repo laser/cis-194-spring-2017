4ac20248a073487b65287e6ff6de7027d0ea07d6
Lindsay Kowalick <rkowalick@gmail.com>
2021-04-01 16:06:41 -0400

    First exercise
    Yay CC Validation
    Yay, towers of hanoi

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,30 +2,37 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits = reverse . toDigitsRev
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n
+  | n <= 0    = []
+  | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
 
 -- #2
+doubleEveryOtherFromFront :: [Integer] -> [Integer]
+doubleEveryOtherFromFront (x:y:rest) = (x:2*y:doubleEveryOtherFromFront rest)
+doubleEveryOtherFromFront list = list
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleEveryOtherFromFront . reverse
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits list = foldr (+) 0 (concatMap toDigitsRev list)
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate = (0 ==) . ((flip mod) 10) . sumDigits . doubleEveryOther . toDigits
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
