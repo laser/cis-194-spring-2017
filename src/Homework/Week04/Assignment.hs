5a7cf7e527dfa311a36b61944f48f804bb82d603
Lindsay Kowalick <rkowalick@gmail.com>
2021-04-07 09:39:44 -0400

    Week 4 Exercises

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -19,72 +19,130 @@ module Homework.Week04.Assignment (
   BST(..)
 ) where
 
+import Data.Char
+import Data.List
+
 import Homework.Week04.BST
+
+safeHead :: [a] -> Maybe a
+safeHead []    = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail []     = Nothing
+safeTail (_:xs) = Just xs
 
 -- #1
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 a b = b
 
 -- #2
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 a1 a2 = a1 -- or a2
 
 -- #3
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 _ a = a
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 _ a1 a2 = a1 -- there are a few implementations here
+-- Impl 1: (always return the first a)
+-- _    a1 a2 = a1
+-- Impl 2: (always returns the second a)
+-- _    a1 a2 = a2
+-- Impl 3: (True passed in returns the first a, False the second
+-- True  a1 a2 = a1
+-- False a1 a2 = a2
+-- Impl 3: (True passed in returns the second a, False the first
+-- True  a1 a2 = a2
+-- False a1 a2 = a1
 
 -- #5
 ex5 :: Bool -> Bool
 ex5 = undefined
+-- Impl 1:
+-- _ -> True
+-- Impl 2:
+-- _ -> False
+-- Impl 3: (Boolean Identity Function)
+-- True -> True
+-- False -> False
+-- Impl 4: (Boolean NOT Function)
+-- True -> False
+-- False -> True
 
 -- #6
 ex6 :: (a -> a) -> a
-ex6 = undefined
+ex6 f = undefined
+-- There is no such function, for one cannot provide an output value
+-- for a function that has no way of being supplied an argument
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 f a = f a
+-- So either apply f or don't
+-- Another Impl (don't apply f)
+-- ex7 _ a = a
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 [] = []
+ex8 (a:as) = (a:as)
+-- There are lots of these, but I think all of them can be regarded function from
+-- a list to a permutation of a subsequence of the list
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
 ex9 = undefined
+-- These will all have the form of (do the mapping) then perform some kind of
+-- operation that would quality as an ex8 from above
 
 -- #10
 ex10 :: Maybe a -> a
 ex10 = undefined
+-- This doesn't appear definable, as there is no way
+-- to select an item for Nothing to map to
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 a = Just a
+-- 2 implementations. Eitehr Just a or Nothing
+-- ex11 a = Just a
+-- ex11 _ = Nothing
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+-- there are two implementations
+-- either the Nothing constant function
+ex12 _ = Nothing
+-- or the identity function
+-- ex12 Just a  = Just a
+-- ex12 Nothing = Nothing
+-- Nothing must map to nothing, and because we know nothing about the type
+-- a, Just a -> Nothing or Just a
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ x Leaf = Node Leaf x Leaf
+insertBST o x (Node left y right) =
+  if o x y == LT
+    then (Node (insertBST o x left) y right)
+    else (Node left y (insertBST o y right))
 
 -- #14
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps list = all ((maybe False isUpper) . safeHead) list
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+-- dropTrailingWhitespace str = undefined
+dropTrailingWhitespace str = reverse $ dropWhile isSpace (reverse str)
 
 -- #16
 firstLetters :: [String] -> [Char]
-firstLetters = undefined
+firstLetters list = map (maybe ' ' id) (filter (maybe False (const True)) (map safeHead list))
 
 -- #17
 asList :: [String] -> String
-asList = undefined
+asList list = "[" ++ (intercalate "," (filter (not . null) list)) ++ "]"
